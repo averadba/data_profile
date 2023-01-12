fd668939e737ce86e0f80a6dfe6976c87865b130
Alexis Vera <76126512+averadba@users.noreply.github.com>
2023-01-11 20:08:02 -0400

    Update streamlit_app.py
    Changed code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,34 @@
 import pandas as pd
 import pandas_profiling
 import streamlit as st
-from contextlib import suppress
 
-from streamlit_pandas_profiling import st_profile_report
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
 
-st.title("Data Profiler")
-st.write("by: A. Vera")
-st.markdown("The data profiler is a simple tool to take a quick glance at your data.")
+@st.cache
+def load_data(file):
+    return pd.read_csv(file)
 
+def main():
+    st.set_page_config(page_title="Data Profiler", page_icon=":chart_with_upwards_trend:", layout="wide")
+    st.title("Data Profiler")
+    st.write("Use this app to generate a quick profile of the data contained in your CSV file.")
 
-uploaded_file = st.file_uploader("Choose a CSV file", type=['csv'], help="Only csv files are accepted. Make sure the first row contains column names.")
-if uploaded_file is not None:
-# To read file as bytes:
-     #bytes_data = uploaded_file.getvalue()
-     df =pd.read_csv(uploaded_file)
-     #st.write(bytes_data)
-     st.write(uploaded_file)
-     #st.dataframe(uploaded_file,200,100)
+    # Select the CSV file
+    file = st.file_uploader("Upload your CSV file", type="csv")
+    if file is None:
+        st.error("Please upload a CSV file.")
+        return
 
+    # Load the data
+    data = load_data(file)
 
+    # Display the data profile
+    st.write("Data Profile")
+    profile = pandas_profiling.ProfileReport(data)
+    st.write(profile)
 
-#df =pd.read_csv(uploaded_file)
-#df = pd.DataFrame(uploaded_file,sep=",")
-
-if st.button("Generate Profile Report"):
-    pr = df.profile_report()
-
-    st_profile_report(pr)
+if __name__== "__main__":
+    main()
